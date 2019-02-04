684f051110c3b74506098196faa10abcac1f8e9a
Rajesh Tiwari <learn4develop@gmail.com>
2019-02-05 00:22:35 +0530

    state with class base component and geolocation example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        }
+        window.navigator.geolocation.getCurrentPosition(
+            latitute => {
+                this.setState({
+                    lat: latitute.coords.latitude
+                })
+            },
+            err => {
+                this.setState({
+                    errorMessage: err.message
+                })
+            }
+        );
+    }
+
+    render() {
+
+        if (this.state.lat && !this.state.errorMessage) {
+            return (
+                <div>
+                    Hi Bro
+                    my latituet is : {this.state.lat}
+                    <br />
+                </div>
+            )
+        } else if (! this.state.lat && this.state.errorMessage){
+            return (
+                <div>
+                    Hi Bro
+                    error Message : {this.state.errorMessage}
+                    <br />
+                </div>
+            )
+
+        }
+        return <div> Loading !!! </div>
+    }
+
+}
+
+export default App;
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
